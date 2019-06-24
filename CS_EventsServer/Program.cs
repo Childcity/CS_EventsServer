763acaedea22291526a54812e85d1440abbe8123
Childcity <skulazkiy@gmail.com>
2019-06-24 12:51:58 +0300

    init project

---
+++ CS_EventsServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_EventsServer {
+	class Program {
+		static void Main(string[] args) {
+		}
+	}
+}
