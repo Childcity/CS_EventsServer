ccdc49019f85b69e70ed3cb63031a1d7bd6b788d
Childcity <skulazkiy@gmail.com>
2019-07-28 01:54:46 +0300

    add Entities

---
+++ CS_EventsServer/Server/DAL/Entities/Account55.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CS_EventsServer.Server.DAL.Entities {
+	
+}
---
+++ CS_EventsServer/Server/DAL/Entities/Event55.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CS_EventsServer.Server.DAL.Entities {
+
+	
+}
